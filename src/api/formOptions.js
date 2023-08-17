3cb1fd1eb15a6a9a9161161bb4b6a9543185f6ae
Franco Andres Diaz <126727423+franad23@users.noreply.github.com>
2023-08-16 21:58:22 -0300

    Add files via upload

---
+++ src/api/formOptions.js
@@ -0,0 +1,90 @@
+import axios from "axios";
+
+const urlBackend = 'http://localhost:3000/api';
+
+export const addFormApi = (form) => {
+
+
+  return axios.post(`${urlBackend}/postForm`, form, {
+    withCredentials: true
+  });
+};
+
+export const gettingFormsUser = () => {
+  return axios.get(`${urlBackend}/getFormuserform`, {
+    withCredentials: true
+  });
+};
+
+export const gettingForm = (idform) => {
+  return axios.get(`${urlBackend}/getFormuserform/${idform}`, {
+    withCredentials: true 
+  });
+};
+
+export const gettingFormToResp = (idform) => {
+  return axios.get(`${urlBackend}/getFormtoresp/${idform}`);
+};
+
+export const deleteForm = (idform) => {
+  return axios.delete(`${urlBackend}/deleteform/${idform}`, {
+    withCredentials: true 
+  });
+};
+
+export const updateForm = (idform, form) => {
+  return axios.patch(`${urlBackend}/modifyform/${idform}`, form, {
+    withCredentials: true 
+  });
+};
+
+export const gettingResponses = (idform) => {
+  return axios.get(`${urlBackend}/gettingResponses/${idform}`, {
+    withCredentials: true 
+  });
+}
+
+export const postUserForm = (idform, form) => {
+  return axios.post(`${urlBackend}/postFormToResp/${idform}`, form, {
+    withCredentials: true 
+  });
+}
+
+export const loginUser = (user) => {
+  return axios.post(`${urlBackend}/login`, user, {
+    withCredentials: true 
+  });
+}
+
+export const registerUser = (user) => {
+  return axios.post(`${urlBackend}/register`, user, {
+    withCredentials: true 
+  });
+}
+
+export const logoutuser = () => {
+  return axios.post(`${urlBackend}/logout`, {
+    withCredentials: true 
+  });
+}
+
+export const getUserToVerify = (id) => {
+  return axios.get(`${urlBackend}/toverifyemail/${id}`, {
+    withCredentials: true 
+  });
+}
+export const verifyUser = (id) => {
+  return axios.patch(`${urlBackend}/toverifyemail/${id}`, {
+    withCredentials: true 
+  });
+}
+export const changePassword = (id, password) => {
+  return axios.post(`${urlBackend}/recoverypasswordPage/${id}`, password, {
+    withCredentials: true 
+  });
+}
+export const sendMailTorecoveryPass = (email) => {
+  return axios.post(`${urlBackend}/recoverypassword`, email, {
+    withCredentials: true 
+  });
+}
