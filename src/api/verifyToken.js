db136478d07bddff815ffd26a622c46fa9e0c691
Franco Andres Diaz <126727423+franad23@users.noreply.github.com>
2023-08-17 00:29:23 -0300

    Update verifyToken.js

--- src/api/verifyToken.js
+++ src/api/verifyToken.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const urlBackend = 'https://backendproyfinalrc.onrender.com';
+const urlBackend = 'https://backendproyfinalrc.onrender.com/api';
 
 export const verifyToken = () => {
   const token = document.cookie.split(';').find(cookie => cookie.trim().startsWith('token=')).split('=')[1];
