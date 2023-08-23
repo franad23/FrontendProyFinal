db2996dac849874f98bbf17ad5b788de1554c81a
Franco Andres Diaz <126727423+franad23@users.noreply.github.com>
2023-08-23 14:16:17 -0300

    Delete api directory

--- api/verifyToken.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-const urlBackend = 'http://localhost:3000/api';
-
-export const verifyToken = () => {
-  return axios.get(`${urlBackend}/verify`, {
-    withCredentials: true // Habilitar el envío de cookies
-  });
-}
